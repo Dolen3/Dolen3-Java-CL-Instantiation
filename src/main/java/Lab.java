faa74bb6f8aebdc17b0db55c888bbc5f8fdbb9bd
Daniel Olen <danolen89@gmail.com>
2024-09-09 13:44:38 +0000

    Coding Lab: Instantiation Completed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Lab test = new Lab();
+        return test;
     }
 }
